8d207c46c67824a4adad3f55b68fc51287862d89
k8v <k8v@live.fr>
2026-02-08 19:21:53 +0100

    Refactor categories in countries_tri_playlist.py

--- countries_tri_playlist.py
+++ countries_tri_playlist.py
@@ -10,103 +10,85 @@ OUTPUT_FILE = "generated.m3u"
 
 # --- CONFIGURATION DES CATÉGORIES ---
 CATEGORIES = {
-    "🇫🇷 TNT": [
+CATEGORIES = {
+    "TNT": [
         "tf1", "france2", "france3", "france4", "france5", "m6", "arte", "c8", "w9", 
         "tmc", "tfx", "nrj12", "lcp", "bfmtv", "cnews", "cstar", "gulli", "tf1series", 
         "lequipe", "6ter", "rmcstory", "rmcdecouverte", "cherie25", "lci", "franceinfo"
     ],
-    "🎬 CINÉMA & SÉRIES": [
-        "canalplus", "cineplus", "ocs", "action", "ab1", "rtl9", "teva", "paramount", 
-        "warner", "novelas", "crimedistrict", "serieclub", "syfy", "tvbreizh", "polar", 
-        "comedycentral", "comedie", "studiocanal", "tcm", "persiana", "sony", "justepourrire", 
-        "cordier", "fillesdacote", "cinenanar", "cinewestern", "wildsidetv", "dossiersfbi", 
-        "novo19", "bbcdrama", "degrassi", "heleneetlesgarcons", "lemiracledelamour", 
-        "lesanneesfac", "lesnouveauxdetectives", "louislabrocante", "screamin", "theasylum", 
-        "walkertexasranger", "yaquelaveritequicompte", "instantsaga", "seriemax", "emotionl", "ab3",
-        "intocrime"
+    "Archives": [
+        "archives", "ina", "retro", "classic"
     ],
-    "🧸 JEUNESSE": [
+    "Art de vivre": [
+        "maison", "marmiton", "myzentv", "artdevivre", "deco", "cuisine"
+    ],
+    "Cinéma": [
+        "canalplus", "cineplus", "ocs", "action", "studiocanal", "tcm", "sony", 
+        "cinenanar", "cinewestern", "wildsidetv", "screamin", "theasylum", "emotionl"
+    ],
+    "Culture": [
+        "museum", "culturebox", "expo", "art"
+    ],
+    "Divertissement": [
+        "ab1", "rtl9", "teva", "comedycentral", "comedie", "justepourrire", 
+        "yaquelaveritequicompte", "clique", "mcm", "mta", "enorme", "bet"
+    ],
+    "Documentaires": [
+        "histoire", "planete", "toutehistoire", "investigation", "trek", 
+        "discovery", "rmcdecouverte", "dossiersfbi"
+    ],
+    "Economie": [
+        "bfmbusiness", "bsmart", "tvfinance", "nweconomie", "business24"
+    ],
+    "Généraliste": [
+        "tv5monde", "ab3", "canale", "generaliste", "laune", "rtltvi"
+    ],
+    "Info": [
+        "euronews", "france24", "i24", "figaro", "meteo", "africanews", 
+        "cgtnfrench", "nwinfo", "rtfrance", "lci", "cnews", "bfmtv"
+    ],
+    "Jeunesse": [
         "canalj", "disney", "mangas", "piwi", "nickelodeon", "tiji", "teletoon", 
-        "boomerang", "cartoon", "tivi5", "adn", "ludikids", "caillou", "bobleponge", 
-        "amuse", "bubbleguppies", "mbc3", "angelaanaconda", "avatar", "babyfirst", 
-        "doratv", "inazumaeleven", "sabrina", "tortuesninja", "victorious", "amouyazid"
+        "boomerang", "cartoon", "tivi5", "adn", "ludikids", "caillou", "bobleponge"
     ],
-    "🌍 DÉCOUVERTE & SAVOIR": [
-        "animaux", "histoire", "museum", "natgeo", "planete", "sciencevie", "toutehistoire", 
-        "ushuaia", "montagne", "discovery", "investigation", "chasse", "trek", "seasons", 
-        "ultranature", "maison", "sorcier", "marmiton", "myzentv", "geopolistv", "sosdocteur", 
-        "bebtv", "3abn", "amanitv", "ecclesiatv", "ewtn", "haditv", "iqraa", "manaeglise", 
-        "metanoiatv", "sophiatv", "telepace", "chandel", "divinamour", "dynamicgospel", 
-        "emcitv", "faith", "hope", "kto", "revelation", "shilo", "wisdom", "religion",
-        "mygospeltv", "esaie45tele", "radiotele4veh", "radioteleamen", "radiotelefullgospel", 
-        "radioteleevangile", "televisionespoir47", "naturaltv", "mdl"
+    "Jeux": [
+        "esport", "gaming", "gameone", "nolife"
     ],
-    "📰 INFOS & ÉCONOMIE": [
-        "bfmbusiness", "euronews", "france24", "i24", "figaro", "meteo", "bsmart", 
-        "tvfinance", "africanews", "cgtnfrench", "presstvfrench", "nwinfo", "nweconomie", 
-        "bfm2", "bfmtechco", "tr24television", "rtfrance", "business24", "burkinainfo", "franceinfo",
-        "lemediatv", "publicsenat2424", "tv5mondeinfo", "francophonie24"
+    "Local": [
+        "bx1", "telenantes", "tv7", "biptv", "matele", "tl7", "monacoinfo", "tvmonaco"
     ],
-    "🎶 MUSIQUE & DIVERTISSEMENT": [
-        "mcm", "mezzo", "mtv", "trace", "bblack", "melody", "rfm", "nrjhits", 
-        "cstarhits", "m6music", "mouv", "qwest", "fashion", "clique", "gong", "d5music", 
-        "a2imusic", "clubbingtv", "stingray", "radiokaraoke", "funradio", "generations", "tl7",
-        "a2inaija", "a2itv", "mensuptv", "rtl2", "sudradio", "franceinter", "radiofrontieres", 
-        "arabel", "handicaptv", "rmclife", "tzik", "zenith", "novocomedy"
+    "Musique": [
+        "mezzo", "mtv", "trace", "bblack", "melody", "rfm", "nrjhits", 
+        "cstarhits", "m6music", "mouv", "clubbingtv", "stingray"
     ],
-    "📍 RÉGIONALES & LOCALES": [
-        "20minutestv", "t18", "canalalpha", "7alimoges", "8montblanc", "alsace20", "astv", "biptv", "telenantes", 
-        "tv7", "vosges", "kto", "canal32", "weo", "tebeo", "tebesud", "grandgeneve", "tvr", 
-        "matele", "tl7", "canalzoom", "cannes", "nancy", "tv78", "iltv", "telegohelle", 
-        "tv3v", "rhonetv", "telebielingue", "nrtv", "bluezoomf", "qu4treliegemedia", 
-        "telemb", "tvlux", "angers", "alpedhuez", "brionnais", "monacoinfo", "tvmonaco", 
-        "vedia", "viaoccitanie", "viatelepaese", "bfmalsace", "bfmcotedazur", "bfmdici", 
-        "bfmgrandlille", "bfmgrandlittoral", "bfmlyon", "bfmmarseille", "bfmnormandie", 
-        "bfmvar", "chamber", "latere", "maxtv", "carac", "tma", "rht", "basseterre", "icielsass", "montecarlodigitaltelevision"
+    "Nature": [
+        "animaux", "natgeo", "ultranature", "seasons", "chasse", "peche", "wild"
     ],
-    "⚽ SPORTS": [
+    "Régional": [
+        "france3", "alsace20", "weo", "tebeo", "bfmalsace", "bfmlyon", "bfmmarseille", "viatelepaese"
+    ],
+    "Reportages": [
+        "echappeesbelles", "reportages", "envoyespecial"
+    ],
+    "Sciences": [
+        "sciencevie", "discovery", "explora", "curiosity"
+    ],
+    "Séries-Films": [
+        "paramount", "warner", "novelas", "crimedistrict", "serieclub", "syfy", 
+        "tvbreizh", "polar", "cordier", "fillesdacote", "bbcdrama", "degrassi", 
+        "heleneetlesgarcons", "lemiracledelamour", "seriemax", "intocrime"
+    ],
+    "Sociétal": [
+        "kto", "religion", "emcitv", "ewtn", "iqraa", "handicaptv", "publicsenat"
+    ],
+    "Sport": [
         "sport", "bein", "eurosport", "equidia", "automoto", "rmcsport", "golf", 
-        "multisports", "footplus", "fighting", "nhlcentreice", "journaldugolf", "nautical", "failarmy"
+        "footplus", "fighting", "nhl", "lequipe"
     ],
-    "🇧🇪 BELGIQUE": [
-        "laune", "ladeux", "latrois", "rtltvi", "clubrtl", "plugrtl", "ln24", "tipik", 
-        "bx1", "bouke", "bruzz", "belrtl", "rtlzwee", "ab3", "canalz", "radiocontact", "actv"
-    ],
-    "🇨🇭 SUISSE": [
-        "rtsun", "rtsdeux", "srfinfo", "tvm3", "lemanbleu", "couleur3", "rts1", "rts2", 
-        "canal9", "carac", "la-tele", "maxtv", "nrj-leman", "kanal9", "latele"
-    ],
-    "🇨🇦 CANADA / QUÉBEC": [
-        "radiocanada", "icitelea", "icirdi", "tva", "noovo", "lcn", "telequebec", "tcftv", 
-        "cbaf", "cbft", "cbkf", "cblf", "cboft", "cbuft", "cbvt", "cbwft", "cftmdt", "cftudt", 
-        "civmdt", "cjbrdt", "ckshdt", "cktmdt", "cktvdt", "ctbtv", "assemblee", "legislative",
-        "canaldelassemblee", "montrealgreektv", "tvctk"
-    ],
-    "🌍 AFRIQUE & DOM-TOM": [
-        "atv", "canal3", "cna", "kc2", "ntv", "rtvc", "tvlacapitale", "mta9africa",
-        "aplus", "africa24", "africanews", "nollywood", "rtb", "rti", "ortm", "2mmonde", 
-        "antennereunion", "2stv", "tfm", "sentv", "nci", "lifetv", "canal2", "benietv", 
-        "beninwebtv", "bossbrotherstv", "cbctv", "ccpvtelevision", "centelevision", "crtv", 
-        "d3tv", "dntv", "edentv", "equinoxtv", "evitv", "exploitstv", "foryoutv", "jostvhd", 
-        "kin24", "lbfdrtv", "mbc1", "mbc5", "metropole", "misectv", "mouridetv", "onetv", 
-        "plextv", "publicsntv", "reflettv", "rewmitv", "rtd4", "rtg1", "rtg2", "rtnc", 
-        "rtnc3", "rtvs1", "senewebtv", "senjeunestv", "telecongo", "telesud", "tm1tv", 
-        "tnh", "tv2", "tvcbenin", "tvt", "vision4", "yegletv", "etv", "fusiontv", "tntv", 
-        "telepeyi", "a12tv", "actv.tg", "adotv", "afrique54", "afromagic", "afroturk", 
-        "antennea", "cam10", "canaf54", "chabibatv", 
-        "championtv", "cheriflatv", "compassiontv", "congoplanet", "degatv", 
-        "diaspora24", "digitalcongo", "douniatv", "fasso", "haitinews", "hmipromz", 
-        "identite", "ivoirechannel", "kaback", "kajou", "kalac", "lauradave", "lougatv", 
-        "madertv", "medi1tv", "mikuba", "mytv", "nazalis", "nessma", "nietatv", "novelachannel", 
-        "numerica", "nwmagazine", "onenation", "playtv", "pstvhd", "pvs", "rtjva", 
-        "rwanda", "saraounia", "smatogo", "storychannel", "sunulabel", "taltv", "teleboston", 
-        "telehaiti", "labrise", "telelouange", "telemaroc", "telemasters", "telemusik", 
-        "telepacific", "telepam", "telesahel", "teletchad", "televariete", "telezoukla", 
-        "tempoafric", "viaatv", "voxafrica", "walftv", "yakaartv", "zeeone", "zee-one",
-        "mta1world", "mta2europe", "mta4africa", "mta8africa", "otv",
-        "turkmenistan", "tv5mondeasia", "tv5mondeeurope", "tv5mondefrance", "tv5mondelatin", 
-        "tv5mondepacific", "tv5mondestyle", "tvcarib", "tvfamille", "radiotele", "telemix", "rthtv1", "mta5africa"
-    ],
+    "Voyage": [
+        "voyage", "ushuaia", "montagne", "travelxp", "echappeesbelles", "ailleurs"
+    ]
+}
     "📺 PLUTO TV": [],
     "📺 SAMSUNG TV PLUS": [],
     "📺 RAKUTEN TV": [],
