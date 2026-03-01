6e24b157d59c45501b6c2574b70ba598faf14d71
k8v <k8v@live.fr>
2026-03-01 18:48:58 +0100

    Refactor script execution order and update comments

--- fusion.py
+++ fusion.py
@@ -1,23 +1,25 @@
 import requests
 
-# Remplace par les URLs RAW (bouton 'Raw' sur GitHub) de TES scripts
-URL_SCRIPT_ZAP  = "https://raw.githubusercontent.com/k8v/OMG-Premium-TV/main/tri_zap"
+# URLs RAW de tes scripts sur GitHub
 URL_SCRIPT_PAYS = "https://raw.githubusercontent.com/k8v/OMG-Premium-TV/main/countries_tri_playlist.py"
+URL_SCRIPT_ZAP  = "https://raw.githubusercontent.com/k8v/OMG-Premium-TV/main/tri_zap"
 
 def exec_remote_script(url):
-    print(f"--- Récupération de : {url.split('/')[-1]} ---")
+    print(f"--- Recupération de : {url.split('/')[-1]} ---")
     try:
         response = requests.get(url)
         response.raise_for_status()
-        # On exécute le code python téléchargé
+        # On exécute le code python téléchargé dans l'espace global
         exec(response.text, globals())
-        print(f"Succès : {url.split('/')[-1]} exécuté.\n")
+        print(f"Succès : {url.split('/')[-1]} execute.\n")
     except Exception as e:
         print(f"Erreur avec {url} : {e}")
 
 if __name__ == "__main__":
-    # 1. On lance les trie
-      exec_remote_script(URL_SCRIPT_ZAP) 
-      exec_remote_script(URL_SCRIPT_PAYS)
+    # 1. On lance d'abord le tri par pays
+    exec_remote_script(URL_SCRIPT_ZAP)
     
-    print("Traitement global terminé.")
+    # 2. On lance ensuite le tri zap
+    exec_remote_script(URL_SCRIPT_PAYS)
+    
+    print("Traitement global termine.")
